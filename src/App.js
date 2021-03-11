6aed0bda85f9e8281bd9fa3ee267a70c19d63e27
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-11 21:06:41 +0000

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import SignUp from './components/SignUp';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <SignUp />
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
